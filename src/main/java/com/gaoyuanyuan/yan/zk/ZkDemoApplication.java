87d0fb75d5744684f4bfffd221caf1c3af40834a
yanguowei <yanguow@133.cn>
2018-10-12 18:43:35 +0800

    init

---
+++ src/main/java/com/gaoyuanyuan/yan/zk/ZkDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gaoyuanyuan.yan.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkDemoApplication.class, args);
+	}
+}
